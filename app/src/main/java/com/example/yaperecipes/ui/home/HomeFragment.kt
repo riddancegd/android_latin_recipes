1337ec52b4d1cbe4810af207e01c4e4478f6e5e1
Jhi <riddance.gd@gmail.com>
2024-04-26 12:07:01 -0600

    Feature: Recyclerview search function added

--- app/src/main/java/com/example/yaperecipes/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/yaperecipes/ui/home/HomeFragment.kt
@@ -1,51 +1,69 @@
 package com.example.yaperecipes.ui.home
 
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.SearchView
+import androidx.core.view.isNotEmpty
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.example.yaperecipes.R
 import com.example.yaperecipes.data.model.Recipe
 import com.example.yaperecipes.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
     private val viewModel: HomeViewModel by viewModels()
     private lateinit var recipeAdapter: RecipeAdapter
-    private var recipeList = emptyList<Recipe>()
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        return binding.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupRecyclerView()
-        observeRecipes()
-        setupSearchView()
+
+        val binding = FragmentHomeBinding.bind(view)
+
+        binding.let {
+            setupRecyclerView(it)
+            observeRecipes()
+            setupSearchView(it)
+            fillRecyclerView(it)
+        }
+
     }
 
-    private fun setupRecyclerView() {
+    private fun setupRecyclerView(binding: FragmentHomeBinding) {
         recipeAdapter = RecipeAdapter(emptyList(), this::navigateToDetailFragment)
         binding.recyclerview.adapter = recipeAdapter
     }
 
     private fun observeRecipes() {
         viewModel.recipes.observe(viewLifecycleOwner) { recipes ->
-            recipeList = recipes
             recipeAdapter.submitList(recipes)
+        }
+    }
+
+    private fun setupSearchView(binding: FragmentHomeBinding) {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                recipeAdapter.filter.filter(newText)
+                return true
+            }
+        })
+    }
+
+    private fun fillRecyclerView(binding: FragmentHomeBinding) {
+        viewModel.recipes.value?.let {
+            if (it.isNotEmpty()) {
+                recipeAdapter.submitList(it)
+            }
+            if (binding.searchView.isNotEmpty()) {
+                recipeAdapter.filter.filter(binding.searchView.query.toString())
+            }
         }
     }
 
@@ -54,29 +72,4 @@ class HomeFragment : Fragment() {
         findNavController().navigate(action)
     }
 
-
-    private fun setupSearchView(){
-
-        Log.d("LAUNCHED", "ola k ase")
-
-        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                val recipeFiltered = recipeList.filter { recipeFilter ->
-                    recipeFilter.name.lowercase().contains(newText.toString().lowercase())
-                }
-                recipeAdapter.submitList(recipeFiltered)
-                return true
-            }
-        })
-
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
--- app/src/main/java/com/example/yaperecipes/ui/home/RecipeAdapter.kt
+++ app/src/main/java/com/example/yaperecipes/ui/home/RecipeAdapter.kt
@@ -2,6 +2,8 @@ package com.example.yaperecipes.ui.home
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.yaperecipes.data.model.Recipe
@@ -10,7 +12,9 @@ import com.example.yaperecipes.databinding.RecipeListItemBinding
 class RecipeAdapter(
     private var recipes: List<Recipe>,
     private val onClick: (Recipe) -> Unit
-) : RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>() {
+) : RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>(), Filterable {
+
+    var filteredRecipes: List<Recipe> = recipes
 
     class RecipeViewHolder(private val binding: RecipeListItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -28,18 +32,38 @@ class RecipeAdapter(
     }
 
     override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
-        holder.bind(recipes[position], onClick)
+        holder.bind(filteredRecipes[position], onClick)
     }
 
-    override fun getItemCount(): Int = recipes.size
-
-    fun setFilteredList(recipes: List<Recipe>){
-        this.recipes = recipes
-        notifyDataSetChanged()
-    }
+    override fun getItemCount(): Int = filteredRecipes.size
 
     fun submitList(newRecipes: List<Recipe>) {
         recipes = newRecipes
+        filteredRecipes = newRecipes
         notifyDataSetChanged()
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val filteredList = if (constraint.isNullOrBlank()) {
+                    recipes
+                } else {
+                    val filterPattern = constraint.toString().lowercase().trim()
+                    recipes.filter {
+                        it.name.lowercase().contains(filterPattern) ||
+                                it.ingredients.any { ingredient ->
+                                    ingredient.name.lowercase().contains(filterPattern)
+                                }
+                    }
+                }
+                return FilterResults().apply { values = filteredList }
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                filteredRecipes = results?.values as List<Recipe>
+                notifyDataSetChanged()
+            }
+        }
     }
 }
